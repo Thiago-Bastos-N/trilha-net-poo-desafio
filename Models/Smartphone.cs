84750240d5a6a4decac8b85658dd142a71f0c8b0
Thiago Bastos <t.bastosneves@gmail.com>
2023-12-14 13:51:33 -0300

    trilha-net-desafio-poo

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,15 +2,24 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+        // Propriedade pública
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        // Propriedades privadas
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+
+        // Construtor
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
+        // Métodos existentes
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
@@ -21,6 +30,7 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
+        // Método abstrato
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Instanciando e testando a classe Nokia
+var nokia = new Nokia("123456789", "ModeloNokia", "IMEINokia", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+// Instanciando e testando a classe Iphone
+var iphone = new Iphone("987654321", "ModeloIphone", "IMEIIphone", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
